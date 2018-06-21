31252a829cd5acbfdc2892bd196121ece7713b3c
Aida Younesi <Aida.Younesi@softwire.com>
2018-06-21 18:02:18 +0100

    css and make it nice

--- frontend/departures.js
+++ frontend/departures.js
@@ -1,6 +1,12 @@
 var domain = 'http://localhost:3000'
 
+/**
+ * To be called when the post code form is submitted
+ */
 function setBusStopsArrivals() {
+    showLoader();
+    hideResult();
+
     var postCode = document.postCodeForm.postcode.value;
 
     var xhttp = new XMLHttpRequest();
@@ -10,26 +16,20 @@ function setBusStopsArrivals() {
     xhttp.onload = function() {
 
         var result = document.getElementById('results');
-        removeAllChildren(result);
 
-        console.log(this.responseText);
+        removeAllChildren(result);
+        hideLoader();
 
         if (xhttp.status === 200)
             displayBusStopsArrivals(result,
                                 JSON.parse(this.responseText));
         else
             displayError(result, this.responseText);
+
+        showResult();
     };
     xhttp.send();
 }
-
-function removeAllChildren(htmlElement) {
-    // remove all children
-    while (htmlElement.firstChild) {
-        htmlElement.removeChild(htmlElement.firstChild);
-    }
-}
-
 
 /**
  * @param htmlElement the html element that bus stops should be added to
@@ -37,36 +37,59 @@ function removeAllChildren(htmlElement) {
  */
 function displayBusStopsArrivals(htmlElement, stopsJson) {
 
-    var title = document.createElement('h2');
-    title.innerText = 'Results';
-    htmlElement.appendChild(title);
+    if (stopsJson.length === 0)
+        displayError(htmlElement, 'No bus stop nearby!');
 
     for (var i = 0; i < stopsJson.length; i++) {
-        var heading = document.createElement('h3');
+
+        // using a divs to be able to style them
+        var stop = document.createElement('div');
+        if (i % 2 === 0)
+            stop.classList.add('rightcolumn');
+        else
+            stop.classList.add('leftcolumn');
+
+        var heading = document.createElement('div');
         heading.innerText = 'Stop ' + stopsJson[i].id;
-        htmlElement.appendChild(heading);
+        heading.classList.add('headersmall');
+        stop.appendChild(heading);
 
         var arrivals = stopsJson[i].arrivals;
-        var unorderdeBullets = document.createElement('ul');
-        htmlElement.appendChild(unorderdeBullets);
+        var table = document.createElement('table');
+
+        //add first row or header
+        var header = table.createTHead();
+        var row = header.insertRow();
+        var cell = row.insertCell();
+        cell.innerText = 'Bus';
+        cell = row.insertCell();
+        cell.innerText = 'minutes';
+
+        //add other rows, one row per arrival
+        var body = table.createTBody();
         for (var j = 0; j < arrivals.length; j++) {
-            var item = document.createElement('li');
-            item.innerText = Math.round(arrivals[j].timeToStation/60)+' minutes: ' +
-                            arrivals[j].lineName +' to '+ arrivals[j].destinationName;
-            unorderdeBullets.appendChild(item);
+            var tr =body.insertRow();
+
+            var td = tr.insertCell();
+            td.innerText = arrivals[j].lineName +' towards '+ arrivals[j].destinationName;
+
+            td = tr.insertCell();
+            var minutes = Math.round(arrivals[j].timeToStation/60);
+            if (minutes === 0)
+                minutes = 'Due';
+            td.innerText = minutes;
         }
+        stop.appendChild(table);
+        htmlElement.appendChild(stop);
     }
 }
 
-function navigateToHistory() {
-    window.location.href = `${domain}/history`;
-}
-
-
+/**
+ * To be called on loading of blog page
+ */
 function setHistoryBlog() {
     resp = [{'date':'12 Dec 2017', 'title':'Title1', 'description': 'Description 123', 'image': 'https://www.softwire.com/wp-content/uploads/2018/02/david-lloyd.jpg'},
-        {'date':'16 Jan 2018', 'title':'Title2', 'description': 'Description 456', 'image':'https://www.softwire.com/wp-content/uploads/2018/02/bbc.jpg'},
-        {'date':'5 Dec 2017', 'title':'Title3', 'description': 'Description 789'}];
+        {'date':'16 Jan 2018', 'title':'Title2', 'description': 'Description 456', 'image':'https://www.softwire.com/wp-content/uploads/2018/02/bbc.jpg'}];
 
     // var xhttp = new XMLHttpRequest();
 
@@ -85,8 +108,15 @@ function setHistoryBlog() {
  */
 function displayPosts(htmlElement, postsDataJson) {
     for (var post in postsDataJson) {
-        console.log(postsDataJson[post]);
-        displayOnePost(htmlElement, postsDataJson[post]);
+        // using a divs to be able to style them
+        var postElement = document.createElement('div');
+        if (post % 2 === 0)
+            postElement.classList.add('rightcolumn');
+        else
+            postElement.classList.add('leftcolumn');
+
+        displayOnePost(postElement, postsDataJson[post]);
+        htmlElement.appendChild(postElement);
     }
 }
 
@@ -97,6 +127,7 @@ function displayPosts(htmlElement, postsDataJson) {
 function displayOnePost(htmlElement, postDataJson) {
     var newChild = document.createElement('h2');
     newChild.innerText = postDataJson.title;
+    newChild.classList.add('card');
     htmlElement.appendChild(newChild);
 
     newChild = document.createElement('h5');
@@ -120,6 +151,38 @@ function displayOnePost(htmlElement, postDataJson) {
  */
 function displayError(htmlElement, error) {
     var newChild = document.createElement('h1');
+    newChild.classList.add('header');
     newChild.innerText = 'Ooops! ' + error;
     htmlElement.appendChild(newChild);
+}
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+function showLoader() {
+    document.getElementById("loader").style.display = "block";
+}
+
+function hideLoader() {
+    document.getElementById("loader").style.display = "none";
+}
+
+function showResult() {
+    document.getElementById("results").style.display = "block";
+}
+
+function hideResult() {
+    document.getElementById("results").style.display = "none";
+}
+
+function removeAllChildren(htmlElement) {
+    // remove all children
+    while (htmlElement.firstChild) {
+        htmlElement.removeChild(htmlElement.firstChild);
+    }
 }
